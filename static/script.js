3fa10659b325526a5ae9e6e7d19471fc74658b38
Chris McKeel <chrisfox9158@gmail.com>
2026-07-21 01:06:43 -0400

    Add renderCards(), getDealerCardsArray(), and dealer/player card rendering wiring

--- static/script.js
+++ static/script.js
@@ -4,6 +4,11 @@ fetch("/state")
     .then(data => {
         console.log(data);
         updateBanner(data);
+    
+        // Render cards for state
+        const dealerCards = getDealerCardsArray(data.dealer_state);
+        renderCards(dealerCards, document.getElementById("dealer-cards"));
+        renderCards(data.seats[0].hands[0].cards, document.getElementsByClassName("cards")[0])
     });
 
 // Info banner update function
@@ -16,5 +21,31 @@ function updateBanner(data) {
         banner.textContent = "Your turn";
     } else {
         banner.hidden = true;
+    }
+}
+
+// Card render function
+function renderCards(cardsArray, containerElement) {
+    containerElement.innerHTML = "";
+    for (const card of cardsArray) {
+        const cardElement = document.createElement("div");
+        cardElement.textContent = card.rank_display + " of " + card.suit;
+        containerElement.appendChild(cardElement);
+    }
+}
+
+// Dealer hand translator (with placeholder hole-card)
+function getDealerCardsArray(dealerState) {
+    let cardsArray = [];
+    if (dealerState.showing_card === null) {
+        return cardsArray;
+    } else if (dealerState.hole_card === null) {
+        cardsArray.push(dealerState.showing_card);
+        cardsArray.push({rank_display: "?", suit: "hidden_card"});
+        return cardsArray;
+    } else {
+        cardsArray.push(dealerState.showing_card);
+        cardsArray.push(dealerState.hole_card);
+        return cardsArray;
     }
 }
