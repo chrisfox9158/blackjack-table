d59485b1a87e00f9d595a52ffa9b9d991059a821
Chris McKeel <chrisfox9158@gmail.com>
2026-07-21 00:31:36 -0400

    Add info banner update function and add to state fetch

--- static/script.js
+++ static/script.js
@@ -1,5 +1,20 @@
+// Primary state fetching
 fetch("/state")
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        updateBanner(data);
     });
+
+// Info banner update function
+function updateBanner(data) {
+    const banner = document.getElementById("message-banner");
+    banner.hidden = false;
+    if (data.round_phase === "BETTING") {
+        banner.textContent = "Place your bet";
+    } else if (data.round_phase === "PLAYER_TURN") {
+        banner.textContent = "Your turn";
+    } else {
+        banner.hidden = true;
+    }
+}
